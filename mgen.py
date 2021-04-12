b747e7b2b134dd2eebba65ac5c64c641b9371836
jordiugarte <48989035+jordiugarte@users.noreply.github.com>
2021-04-11 21:21:00 -0400

    Initial

--- mgen.py
+++ mgen.py
@@ -6,10 +6,10 @@ from constants.notes import *
 def metronome(duration, frequency):
     beat = 0
     while beat <= duration:
-        FILE.addNote(TRACK, CHANNEL, G5, beat, SEMIFUSA, VOLUME)
+        FILE.addNote(TRACK, CHANNEL, A3, beat, SEMIFUSA, VOLUME)
         beat += frequency
     with open("output.mid", 'wb') as outf:
         FILE.writeFile(outf)
 
 #   Duration in
-metronome(10, 1)
+metronome(30, 1)
