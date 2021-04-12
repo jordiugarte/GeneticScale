cc15fb8ce4d12c04cefcf0c4cc148364da368778
jordiugarte <48989035+jordiugarte@users.noreply.github.com>
2021-04-11 21:18:49 -0400

    Initial

---
+++ constants/default_settings.py
@@ -0,0 +1,9 @@
+from midiutil import MIDIFile
+
+FILE = MIDIFile(1)
+TRACK = 0
+TIME = 0
+FILE.addTrackName(TRACK, TIME, "Sample Track")
+FILE.addTempo(TRACK, TIME, 120)
+CHANNEL = 0
+VOLUME = 100
--- mgen.py
+++ mgen.py
@@ -1,28 +1,15 @@
-from midiutil.MidiFile import MIDIFile
+from constants.default_settings import *
+from constants.figures import *
+from constants.notes import *
 
-from notes import *
-mf = MIDIFile(1)
-track = 0
-time = 0
-mf.addTrackName(track, time, "Sample Track")
-mf.addTempo(track, time, 120)
-channel = 0
-volume = 100
 
-pitch = G5
-time = 0
-duration = 1
-mf.addNote(track, channel, pitch, time, duration, volume)
+def metronome(duration, frequency):
+    beat = 0
+    while beat <= duration:
+        FILE.addNote(TRACK, CHANNEL, G5, beat, SEMIFUSA, VOLUME)
+        beat += frequency
+    with open("output.mid", 'wb') as outf:
+        FILE.writeFile(outf)
 
-pitch = Bb5
-time = 2
-duration = 1
-mf.addNote(track, channel, pitch, time, duration, volume)
-
-pitch = C5
-time = 4
-duration = 1
-mf.addNote(track, channel, pitch, time, duration, volume)
-
-with open("output.mid", 'wb') as outf:
-    mf.writeFile(outf)
+#   Duration in
+metronome(10, 1)
